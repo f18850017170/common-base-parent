78c529463dc7ca2d508ccca95713f9270140219e
fengjl <fengjianglong@mashanghong.com>
2018-07-13 11:51:19 +0800

    common-web指定日志文件地址 MultipartConfig.java 上传文件临时地址修改

--- common-web/src/main/java/cn/vonfly/config/MultipartConfig.java
+++ common-web/src/main/java/cn/vonfly/config/MultipartConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.MultipartConfigElement;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class MultipartConfig {
@@ -16,12 +18,13 @@ public class MultipartConfig {
 	@Bean
 	MultipartConfigElement multipartConfigElement() {
 		MultipartConfigFactory factory = new MultipartConfigFactory();
-		String location = System.getProperty("user.dir") + "/data/tmp";
-		File tmpFile = new File(location);
+		Path path = Paths.get(System.getProperty("user.dir"));
+		Path temp = path.resolveSibling("data/tmp");
+		File tmpFile = temp.toFile();
 		if (!tmpFile.exists()) {
 			tmpFile.mkdirs();
 		}
-		factory.setLocation(location);
+		factory.setLocation(temp.toString());
 		return factory.createMultipartConfig();
 	}
 }
